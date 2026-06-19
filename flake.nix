365376e6f6b6361d66ae20acc89c8f9816602f90
tacascer <trandangtrithanh2000@gmail.com>
2026-06-19 23:42:13 +0100

    Initialize Gradle Kotlin flake project

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Development environment for toml-formatter";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = [
+          pkgs.jdk25
+          pkgs.gradle_9
+        ];
+      };
+    };
+}
