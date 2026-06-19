bd9237547d0aa4f108f8c925de476f23127ad317
tacascer <trandangtrithanh2000@gmail.com>
2026-06-20 00:20:47 +0100

    Add Rust WASM dev shell toolchain

--- flake.nix
+++ flake.nix
@@ -3,18 +3,34 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { nixpkgs, ... }:
+  outputs = { nixpkgs, rust-overlay, ... }:
     let
       system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ rust-overlay.overlays.default ];
+      };
+      rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+        extensions = [
+          "rustfmt"
+          "clippy"
+        ];
+        targets = [ "wasm32-unknown-unknown" ];
+      };
     in
     {
       devShells.${system}.default = pkgs.mkShell {
         packages = [
           pkgs.jdk25
           pkgs.gradle_9
+          rustToolchain
         ];
       };
     };
