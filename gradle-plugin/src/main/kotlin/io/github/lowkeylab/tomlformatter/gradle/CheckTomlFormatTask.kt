c7a7b0b1cf0f4cb8718b2adf7bac875c826c438f
tacascer <trandangtrithanh2000@gmail.com>
2026-07-24 22:03:14 +0100

    feat: cache TOML format checks

--- gradle-plugin/src/main/kotlin/io/github/lowkeylab/tomlformatter/gradle/CheckTomlFormatTask.kt
+++ gradle-plugin/src/main/kotlin/io/github/lowkeylab/tomlformatter/gradle/CheckTomlFormatTask.kt
@@ -1,18 +1,20 @@
 package io.github.lowkeylab.tomlformatter.gradle
 
+import java.io.File
 import org.gradle.api.DefaultTask
 import org.gradle.api.GradleException
 import org.gradle.api.file.ConfigurableFileCollection
 import org.gradle.api.file.DirectoryProperty
+import org.gradle.api.tasks.CacheableTask
 import org.gradle.api.tasks.IgnoreEmptyDirectories
 import org.gradle.api.tasks.InputFiles
 import org.gradle.api.tasks.Internal
+import org.gradle.api.tasks.OutputFile
 import org.gradle.api.tasks.PathSensitive
 import org.gradle.api.tasks.PathSensitivity
 import org.gradle.api.tasks.TaskAction
-import org.gradle.work.DisableCachingByDefault
 
-@DisableCachingByDefault(because = "Initial check task has no declared output snapshot")
+@CacheableTask
 public abstract class CheckTomlFormatTask : DefaultTask() {
     @get:InputFiles
     @get:IgnoreEmptyDirectories
@@ -21,8 +23,14 @@ public abstract class CheckTomlFormatTask : DefaultTask() {
 
     @get:Internal public abstract val projectDirectory: DirectoryProperty
 
+    @get:OutputFile
+    protected val successMarker: File
+        get() = temporaryDir.resolve("success.txt")
+
     @TaskAction
     public fun check() {
+        successMarker.delete()
+
         val projectDir = projectDirectory.asFile.get()
         val files = resolveSourceFiles(projectDir, sourceFiles)
         val unformattedFiles =
@@ -31,5 +39,7 @@ public abstract class CheckTomlFormatTask : DefaultTask() {
         if (unformattedFiles.isNotEmpty()) {
             throw GradleException(tomlFormatFailureMessage(unformattedFiles, projectDir))
         }
+
+        successMarker.writeText("")
     }
 }
